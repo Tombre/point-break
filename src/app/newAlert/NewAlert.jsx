aad586df66d219912b0e45be9e81f2923659982d
Phuong Huynh <huynhpt@gmail.com>
2016-07-31 05:47:09 +0800

    add styles for create form

--- src/app/newAlert/NewAlert.jsx
+++ src/app/newAlert/NewAlert.jsx
@@ -11,20 +11,53 @@ export default React.createClass({
 	},
 
 	render() {
-		return <div className="newAlert">
-				
-			<Link to={'/alerts'}>X</Link>
+		return <div className="newAlert form dialog">
+				<div className="dialog__header">
+					<div className="dialog__header--left">
+						<Link to={'/alerts'} className="button__close dark">X</Link>
+					</div>
+				</div>
+				<div className="dialog__body">
+					<TextInput placeholder='New Alert Title' type="large" />
 
-			<TextInput placeholder='New Alert Title' type="large" />
+					<TextInput placeholder='Description of the alert' type="small" />
 
-			<div className="inputList">
-				<div className="inputList__item">
-					<label className="inputList__item__label">Activity</label>
-					<SelectInput className="inputList__item__input" options={['Surfing', 'Fishing']} />
+					<div className="selectField">
+						<label>Location</label>
+						<select>
+							<option>1km near Cottesloe</option>
+							<option>2km near Cottesloe</option>
+							<option>5km near Cottesloe</option>
+							<option>10km near Cottesloe</option>
+							<option>20km near Cottesloe</option>
+						</select>
+					</div>
+
+					<div className="selectField">
+						<label>Skill Level</label>
+						<select>
+							<option>Beginner</option>
+							<option>Intermidiate</option>
+							<option>Advanced</option>
+						</select>
+					</div>
+
+					<div className="inputList">
+						<div className="inputList__item">
+							<label className="inputList__item__label">Activity</label>
+							<SelectInput className="inputList__item__input" options={['Surfing', 'Fishing']} />
+						</div>
+					</div>
+
+					<div className="selectField">
+						<label>Swell</label>
+						<select>
+							<option>1ft - 7ft</option>
+						</select>
+					</div>
+
 				</div>
 			</div>
-
-		</div>
 	}
 
 });
