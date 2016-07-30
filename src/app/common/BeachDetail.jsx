e410802e4085c34eea54ce7c4fcd1677c71dc0a3
Phuong Huynh <huynhpt@gmail.com>
2016-07-30 21:16:41 +0800

    add carousel to details page

---
+++ src/app/common/BeachDetail.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/*----------------------------------------------------------
+Component
+----------------------------------------------------------*/
+
+export default React.createClass({
+	propTypes: {
+		alert: React.PropTypes.object
+	},
+	render() {
+		return (			
+			<div className="beachDetail carousel__content">
+				beachDetail
+			</div>
+		);
+	}
+
+});
