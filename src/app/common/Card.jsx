2afeee14e7bbc15aac82138b3f68fb70bdb011e6
Tom Watts <tomcwatts@gmail.com>
2016-07-30 15:28:20 +0800

    Start card component

--- src/app/common/Card.jsx
+++ src/app/common/Card.jsx
@@ -7,9 +7,23 @@ Component
 export default React.createClass({
 
 	render() {
-		return <div className="card">
-			<p>Hello Tom</p>
-		</div>
+		return (
+			<div className="card">
+				<div className="card__bg">
+
+				</div>
+				<div className="card__content">
+					<div className="card__head">
+						<div className="card__heading">
+							LOWER TRESTLES
+						</div>
+						<div className="card__subheading">
+							TRIGG BEACH, WA
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 
 });
