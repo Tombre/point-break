0a405c083dd586e8bd03fe551827eefe7dbd54ef
Tom Watts <tomcwatts@gmail.com>
2016-07-30 20:18:21 +0800

    Style discover weather conditions card

--- src/app/common/Card.jsx
+++ src/app/common/Card.jsx
@@ -12,17 +12,51 @@ export default React.createClass({
 		return (
 			<div className="card">
 				<div className="card__bg">
-					{alert}
 				</div>
 				<div className="card__content">
+
 					<div className="card__head">
-						<div className="card__heading">
+						<div className="card__heading card__heading--center">
 							LOWER TRESTLES
 						</div>
-						<div className="card__subheading">
-							TRIGG BEACH, WA
+					</div>
+
+					<div className="card__body">
+						<div className="card__body-col">
+							<div className="card__body-heading">
+								8 - 12ft
+							</div>
+							<div className="card__body-subheading">
+								Swell
+							</div>
+						</div>
+						<div className="card__body-col">
+							<div className="card__body-heading">
+								NNW
+							</div>
+							<div className="card__body-subheading">
+								Wind
+							</div>
+						</div>
+						<div className="card__body-col">
+							<div className="card__body-heading">
+								13km
+							</div>
+							<div className="card__body-subheading">
+								Wind Speed
+							</div>
 						</div>
 					</div>
+
+					<div className="card__footer">
+						<div className="card__footer-col card__footer-col--left">
+							Surfing
+						</div>
+						<div className="card__footer-col card__footer-col--right">
+							Swimming
+						</div>
+					</div>
+
 				</div>
 			</div>
 		);
