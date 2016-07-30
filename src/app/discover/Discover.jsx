5a62197fa9198b0888f6f71ebef6c821acd66ddc
Tom Leenders <leenders.thomas@gmail.com>
2016-07-30 17:07:35 +0800

    example discover

--- src/app/discover/Discover.jsx
+++ src/app/discover/Discover.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import CardList from 'app/discover/CardList';
+
+const test_data = {
+	alerts: [
+		{
+			name: 'LOWER TRESTLES',
+			beach: 'TRIGG BEACH, WA'
+		},
+		{
+			name: 'EASY SURFING',
+			beach: 'COTTESLOE, WA'
+		}
+	]
+};
 
 export default React.createClass({
-	
+
+	getInitialState() {
+		return test_data;
+	},
+
 	render() {
-		return <div className="page--create">CREATE</div>
+		return <div className="page--create">
+			<CardList alerts={this.state.alerts}>
+		</div>
 	}
 
 });
