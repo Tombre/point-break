d1d3492223a8f3d98fcdbecf0e97d610d6756448
Tom Leenders <leenders.thomas@gmail.com>
2016-07-31 12:25:18 +0800

    connect new alert

--- src/app/newAlert/NewAlert.jsx
+++ src/app/newAlert/NewAlert.jsx
@@ -17,18 +17,18 @@ export default withRouter(connect(null, { getLocation, postNewAlerts })(React.cr
 
 	getInitialState() {
 		return {
-			"title": "Fishing",  
+			"title": "",  
 			"description": "",
 			"location" : [1, 2],
-			"location_range" : 50,
-			"wind_speed": 60,
-			"wind_direction": 60,
-			"rain": 9,
-			"temp": 20,
-			"cloud": 8,
-			"swell_direction": 342,
-			"swell_period": 32,
-			"tide": 16
+			"location_range" : 20,
+			"wind_speed": [1, 20],
+			"wind_direction": [60, 80],
+			"rain": [6, 9],
+			"temp": [1, 20],
+			"cloud": [8, 10],
+			"swell_direction": [10, 342],
+			"swell_period": [6, 32],
+			"tide": [4, 16]
 		};
 	},
 
@@ -38,12 +38,52 @@ export default withRouter(connect(null, { getLocation, postNewAlerts })(React.cr
 
 	handleSend() {
 
+		console.log(this.state);
+
+		if (
+			!this.state.title.length ||
+			!this.state.description.length
+		) {
+			return;
+		}
+
+		
+
 		let onComplete = this.props.router.push(`/alerts/`);
 
-		this.props.postNewAlerts()
+		this.props.postNewAlerts(this.state)
 			.then(onComplete, onComplete);
 	},
+
+	handleDataSet(type, value, changeValue) {
+
+		if (!value) {
+			value = changeValue;
+		}
+
+		let set = {};
+		set[type] = value ;
+		this.setState(set);
+	},
+
 	render() {
+
+		let p = _.partial;
+		const actionMap = {
+			'title': p(this.handleDataSet, 'title', null),
+			'description': p(this.handleDataSet, 'description', null),
+			"location" : p(this.handleDataSet, 'location'),
+			"location_range" : p(this.handleDataSet, 'location_range'),
+			"wind_speed": p(this.handleDataSet, 'wind_speed'),
+			"wind_direction": p(this.handleDataSet, 'wind_direction'),
+			"rain": p(this.handleDataSet, 'rain'),
+			"temp": p(this.handleDataSet, 'temp'),
+			"cloud": p(this.handleDataSet, 'cloud'),
+			"swell_direction": p(this.handleDataSet, 'swell_direction'),
+			"swell_period": p(this.handleDataSet, 'swell_period'),
+			"tide": p(this.handleDataSet, 'tide')
+		}
+
 		return <div>
 					<div className="newAlert form dialog">
 						<div className="dialog__header">
@@ -53,9 +93,9 @@ export default withRouter(connect(null, { getLocation, postNewAlerts })(React.cr
 						</div>
 						<div className="dialog__body">
 
-							<TextInput placeholder='New Alert Title' type="large" />
+							<TextInput placeholder='New Alert Title' type="large" onChange={actionMap.title} />
 
-							<TextInput placeholder='Description of the alert' type="small" />
+							<TextInput placeholder='Description of the alert' type="small" onChange={actionMap['description']} />
 
 							<div className="form__row first">
 								<div className="form__row--label">
@@ -147,7 +187,7 @@ export default withRouter(connect(null, { getLocation, postNewAlerts })(React.cr
 								</div>
 							</div>	
 
-							<button type="submit" className="button__submit">Create New Alert</button>								
+							<button type="submit" className="button__submit" onClick={this.handleSend}>Create New Alert</button>								
 
 						</div>
 					</div>					
